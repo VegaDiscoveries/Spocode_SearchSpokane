16b55abe9de2c08e61e3ac7b38ae5a38ee4ca37e
VegaDiscoveries <vegadiscoveries@gmail.com>
2013-11-02 15:27:14 -0700

    Initial Load

---
+++ db/RecycleData.js
@@ -0,0 +1,84 @@
+﻿var RecycleTotalsMap = [];
+var RecyclePlasticMap = [];
+var RecycleCardboardMap = [];
+var RecycleNewspaperMap = [];
+var RecycleAluminumMap = [];
+var RecycleTinMap = [];
+var RecycleGlassMap = [];
+var RecycleBatteryMap = [];
+
+function getRecycleDataFromService() {
+    $.ajax({
+        type: "GET",
+        url: "http://communities.socrata.com/resource/65a4-f9k9.json",
+        success: function(msg) {
+            gotRecycleData(msg);
+        }
+    });
+}
+
+function gotRecycleData(msg) {
+    var itm, loc;
+    for (var i = 0; i < msg.length; i++) {
+        itm = msg[i];
+        if (itm.rcy_gross_wt) {
+            loc = new Array;
+            loc.location = new google.maps.LatLng(itm.location.latitude, itm.location.longitude);
+            loc.weight = itm.rcy_gross_wt;
+            RecycleTotalsMap.push(loc);
+        }
+        if (itm.glass_wt_gross) {
+            loc = new Array;
+            loc.location = new google.maps.LatLng(itm.location.latitude, itm.location.longitude);
+            loc.weight = itm.glass_wt_gross;
+            RecycleGlassMap.push(loc);
+        }
+        if (itm.tin_wt_gross) {
+            loc = new Array;
+            loc.location = new google.maps.LatLng(itm.location.latitude, itm.location.longitude);
+            loc.weight = itm.tin_wt_gross;
+            RecycleTinMap.push(loc);
+        }
+        if (itm.cardboard_wt_gross) {
+            loc = new Array;
+            loc.location = new google.maps.LatLng(itm.location.latitude, itm.location.longitude);
+            loc.weight = itm.cardboard_wt_gross;
+            RecycleCardboardMap.push(loc);
+        }
+        if (itm.aluminum_wt_gross) {
+            loc = new Array;
+            loc.location = new google.maps.LatLng(itm.location.latitude, itm.location.longitude);
+            loc.weight = itm.aluminum_wt_gross;
+            RecycleAluminumMap.push(loc);
+        }
+        if (itm.plastic_1_wt_gross) {
+            loc = new Array;
+            loc.location = new google.maps.LatLng(itm.location.latitude, itm.location.longitude);
+            loc.weight = itm.plastic_1_wt_gross;
+            RecyclePlasticMap.push(loc);
+        }
+        if (itm.newspaper_wt_gross) {
+            loc = new Array;
+            loc.location = new google.maps.LatLng(itm.location.latitude, itm.location.longitude);
+            loc.weight = itm.newspaper_wt_gross;
+            RecycleNewspaperMap.push(loc);
+        }
+        if (itm.batteries_wt_gross) {
+            loc = new Array;
+            loc.location = new google.maps.LatLng(itm.location.latitude, itm.location.longitude);
+            loc.weight = itm.batteries_wt_gross;
+            RecycleBatteryMap.push(loc);
+        }
+    }
+    // 
+    heatmapRecycle = new google.maps.visualization.HeatmapLayer({ data: RecycleTotalsMap, dissipating: true });
+    heatmapRecycleGlass = new google.maps.visualization.HeatmapLayer({ data: RecycleGlassMap, dissipating: true });
+    heatmapRecycleTin = new google.maps.visualization.HeatmapLayer({ data: RecycleTinMap, dissipating: true });
+    heatmapRecycleCardboard = new google.maps.visualization.HeatmapLayer({ data: RecycleCardboardMap, dissipating: true });
+    heatmapRecycleAluminum = new google.maps.visualization.HeatmapLayer({ data: RecycleAluminumMap, dissipating: true });
+    heatmapRecyclePlastic = new google.maps.visualization.HeatmapLayer({ data: RecyclePlasticMap, dissipating: true });
+    heatmapRecycleNewspaper = new google.maps.visualization.HeatmapLayer({ data: RecycleNewspaperMap, dissipating: true });
+    heatmapRecycleBatteries = new google.maps.visualization.HeatmapLayer({ data: RecycleBatteryMap, dissipating: true });
+}
+
+
---
+++ db/TrafficData.js
@@ -0,0 +1,91 @@
+﻿var TrafficMap = [];
+var TrafficMapPeak = [];
+var TrafficMapDaily = [];
+var TrafficMapNB = [];
+var TrafficMapSB = [];
+var TrafficMapEB = [];
+var TrafficMapWB = [];
+
+function getTrafficDataFromService() {
+    $.ajax({
+        type: "GET",
+        url: "http://communities.socrata.com/resource/7m8u-erqc.json",
+        success: function(msg) {
+            gotTrafficData(msg);
+        }
+    });
+}
+
+function gotTrafficData(msg) {
+    var itm, loc;
+    var daily, peak, dir;
+    for (var i = 0; i < msg.length; i++) {
+        itm = msg[i];
+        if (itm.latitude) {
+            dir = '';
+            daily = 0;
+            peak = 0;
+            if (itm.direction) {
+                dir = itm.direction;
+            }
+            if (itm.adjusted_awdt) {
+                daily = itm.adjusted_awdt;
+            }
+            if (itm.adjusted_pm_peak_hr_volume) {
+                peak = itm.adjusted_pm_peak_hr_volume;
+            }
+            loc = new Array;
+            loc.location = new google.maps.LatLng(itm.latitude, itm.longitude);
+            loc.weight = daily + peak;
+            TrafficMap.push(loc);
+            if (peak > 0) {
+                loc = new Array;
+                loc.location = new google.maps.LatLng(itm.latitude, itm.longitude);
+                loc.weight = peak;
+                TrafficMapPeak.push(loc);
+            }
+            if (daily > 0) {
+                loc = new Array;
+                loc.location = new google.maps.LatLng(itm.latitude, itm.longitude);
+                loc.weight = daily;
+                TrafficMapDaily.push(loc);
+            }
+            if ((daily + peak) > 0) {
+                if (dir == 'NB') {
+                    loc = new Array;
+                    loc.location = new google.maps.LatLng(itm.latitude, itm.longitude);
+                    loc.weight = daily + peak;
+                    TrafficMapNB.push(loc);
+                }
+                if (dir == 'SB') {
+                    loc = new Array;
+                    loc.location = new google.maps.LatLng(itm.latitude, itm.longitude);
+                    loc.weight = daily + peak;
+                    TrafficMapSB.push(loc);
+                }
+                if (dir == 'EB') {
+                    loc = new Array;
+                    loc.location = new google.maps.LatLng(itm.latitude, itm.longitude);
+                    loc.weight = daily + peak;
+                    TrafficMapEB.push(loc);
+                }
+                if (dir == 'WB') {
+                    loc = new Array;
+                    loc.location = new google.maps.LatLng(itm.latitude, itm.longitude);
+                    loc.weight = daily + peak;
+                    TrafficMapWB.push(loc);
+                }
+            } 
+        }
+    }
+    // 
+    heatmapTraffic = new google.maps.visualization.HeatmapLayer({ data: TrafficMap, dissipating: true });
+    heatmapTrafficPeak = new google.maps.visualization.HeatmapLayer({ data: TrafficMapPeak, dissipating: true });
+    heatmapTrafficDaily = new google.maps.visualization.HeatmapLayer({ data: TrafficMapDaily, dissipating: true });
+    heatmapTrafficNB = new google.maps.visualization.HeatmapLayer({ data: TrafficMapNB, dissipating: true });
+    heatmapTrafficSB = new google.maps.visualization.HeatmapLayer({ data: TrafficMapSB, dissipating: true });
+    heatmapTrafficEB = new google.maps.visualization.HeatmapLayer({ data: TrafficMapEB, dissipating: true });
+    heatmapTrafficWB = new google.maps.visualization.HeatmapLayer({ data: TrafficMapWB, dissipating: true });
+}
+
+
